a8d2ffd349f23aa798a2f79f227938981ab6f66f
borvil <31356001+borvil@users.noreply.github.com>
2019-02-07 16:03:18 +0100

    Update CurrencyExchangeServiceApplication.java

--- src/main/java/boris/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/boris/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -2,6 +2,8 @@ package boris.currencyexchangeservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
 
 
 @EnableDiscoveryClient
