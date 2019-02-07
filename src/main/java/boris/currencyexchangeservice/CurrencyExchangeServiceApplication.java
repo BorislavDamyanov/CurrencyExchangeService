08eef53d6536a1fd21da21afab71f15aae959100
borvil <31356001+borvil@users.noreply.github.com>
2019-02-07 14:32:55 +0100

    Update CurrencyExchangeServiceApplication.java

--- src/main/java/boris/currencyexchangeservice/CurrencyExchangeServiceApplication.java
+++ src/main/java/boris/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -3,6 +3,8 @@ package boris.currencyexchangeservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+@EnableEurekaClient
 @SpringBootApplication
 public class CurrencyExchangeServiceApplication {
 
